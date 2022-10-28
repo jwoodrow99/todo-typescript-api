12d9cd48c95d46b7a3123891528a6f8a995bf743
Jack Woodrow <jackwoodrow99@gmail.com>
2022-10-28 15:59:44 -0400

    list controller

--- src/controllers/ListController.ts
+++ src/controllers/ListController.ts
@@ -1,45 +1,74 @@
 import { Request, Response } from 'express';
 import IRequest from '../interface/IRequest';
+import ListModel from '../models/ListModel';
 
 class ListController {
 	static async index(req: IRequest, res: Response) {
+		let lists = await ListModel.query().where('user_id', req.user.id);
 		res
 			.json({
+				lists: lists,
 				message: 'get lists.',
 			})
 			.status(200);
 	}
 
 	static async show(req: IRequest, res: Response) {
+		let list = await ListModel.query()
+			.where('user_id', req.user.id)
+			.andWhere('id', req.params.id)
+			.first();
 		res
 			.json({
-				id: req.params.id,
+				list: list,
 				message: 'get list.',
 			})
 			.status(200);
 	}
 
 	static async create(req: IRequest, res: Response) {
+		let list = await ListModel.query().insert({
+			user_id: req.user.id,
+			title: req.body.title,
+			description: req.body.description,
+		});
+
 		res
 			.json({
+				list: await ListModel.query().where('id', list[0]).first(),
 				message: 'create list.',
 			})
 			.status(200);
 	}
 
 	static async update(req: IRequest, res: Response) {
+		await ListModel.query()
+			.where('id', req.params.id)
+			.andWhere('user_id', req.user.id)
+			.update({
+				title: req.body.title,
+				description: req.body.description,
+			});
+
 		res
 			.json({
-				id: req.params.id,
+				list: await ListModel.query()
+					.where('id', req.params.id)
+					.andWhere('user_id', req.user.id)
+					.first(),
 				message: 'update list.',
 			})
 			.status(200);
 	}
 
 	static async destroy(req: IRequest, res: Response) {
+		await ListModel.query()
+			.where('id', req.params.id)
+			.andWhere('user_id', req.user.id)
+			.del();
+
 		res
 			.json({
-				id: req.params.id,
 				message: 'delete list.',
 			})
 			.status(200);
