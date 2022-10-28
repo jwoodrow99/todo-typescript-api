9c82ce151ce059ee8d56721be3bee96bd4d04ccc
Jack Woodrow <jackwoodrow99@gmail.com>
2022-10-27 20:08:13 -0400

    Redister route completed

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/UserModel';
 
 class AuthController {
 	static async register(req: Request, res: Response) {
-		res
-			.json({
-				message: 'register.',
-			})
-			.status(200);
+		let checkUser = await User.where('email', req.body.email);
+
+		if (checkUser.length == 0) {
+			await User.insert({
+				name: req.body.name,
+				email: req.body.email,
+				password: bcrypt.hashSync(req.body.password, 10),
+			});
+
+			res
+				.json({
+					message: 'Successfully Registered!',
+				})
+				.status(200);
+		} else {
+			res
+				.json({
+					message: 'Unauthenticated.',
+				})
+				.status(401);
+		}
 	}
 
 	static async login(req: Request, res: Response) {
